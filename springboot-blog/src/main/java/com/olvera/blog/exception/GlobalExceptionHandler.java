5d528a7b06dc3b3c92683115c4dd200905ab8241
olvera93 <olvera9302@outlook.com>
2023-10-02 11:58:40 -0600

    [ADD] handleGlobalException

---
+++ springboot-blog/src/main/java/com/olvera/blog/exception/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.olvera.blog.exception;
+
+import com.olvera.blog.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // handle specific exceptions
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
+            ResourceNotFoundException exception,
+            WebRequest webRequest
+    ) {
+
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler(BlogAPIException.class)
+    public ResponseEntity<ErrorDetails> handleBlogAPIException(
+            ResourceNotFoundException exception,
+            WebRequest webRequest
+    ) {
+
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    // global exeptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(
+            ResourceNotFoundException exception,
+            WebRequest webRequest
+    ) {
+
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
---
+++ springboot-blog/src/main/java/com/olvera/blog/payload/ErrorDetails.java
@@ -0,0 +1,22 @@
+package com.olvera.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ErrorDetails {
+
+    private Date timestamp;
+
+    private String message;
+
+    private String details;
+
+
+
+}
